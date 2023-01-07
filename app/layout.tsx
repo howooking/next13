f3d21198f8fbc151591bb746e185b8e2b0801f8c
howooking <junsgk@gmail.com>
2023-01-08 00:26:10 +0900

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,37 @@
-import './globals.css'
+import "../styles/globals.css";
+
+const header = (
+  <header>
+    <div>
+      <h1>Howoo's Blog</h1>
+      <p>👍Welcome to my Tech blog😁</p>
+      <br />
+    </div>
+  </header>
+);
+
+const footer = (
+  <footer>
+    <div>
+      <br />
+      <h3>Developed by Howoo</h3>
+    </div>
+  </footer>
+);
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        {header}
+        {children}
+        {footer}
+      </body>
     </html>
-  )
+  );
 }
